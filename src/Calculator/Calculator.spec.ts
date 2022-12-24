e1d421d04c0dc2c520cdc807046855cf057d7116
mateus-coder1 <letsgood56@gmail.com>
2022-12-23 22:40:55 -0300

    :white_check_mark: Tests: MT-005 add Calculator test cases

--- src/Calculator/Calculator.spec.ts
+++ src/Calculator/Calculator.spec.ts
@@ -6,4 +6,19 @@ describe('Calculator Module', () => {
     const result = Calculator.sum(1, 2)
     expect(result).toBe(3);
   });
+
+  test('sub: sub 2 - 1 to equal 1', () => {
+    const result = Calculator.sub(2, 1)
+    expect(result).toBe(1);
+  });
+
+  test('mult: mult 2 * 2 to equal 4', () => {
+    const result = Calculator.mult(2, 2)
+    expect(result).toBe(4);
+  });
+
+  test('div: div 2 / 2 to equal 1', () => {
+    const result = Calculator.div(2, 2)
+    expect(result).toBe(1);
+  });
 });
